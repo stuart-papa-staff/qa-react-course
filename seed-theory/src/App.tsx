b3ba791760dc0b1c74108cf8925d2d8ba1b5597e
Stu P <stuart@example.com>
2025-06-17 14:29:23 +0100

    WIP 2

--- seed-theory/src/App.tsx
+++ seed-theory/src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage'
 import ItemsPage from './pages/ItemsPage'
 //import './App.css'
 import itemsData from './itemsData.json';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path="/items" element={<ItemsPage itemsData={itemsData} />} />
+            <Route path='/checkout' element={<CheckoutPage />} />
           </Routes>
         </main>
       </BrowserRouter>
--- seed-theory/src/components/CheckoutItem.tsx
+++ seed-theory/src/components/CheckoutItem.tsx
@@ -1,1 +1,20 @@
-// to follow
+type CheckoutItemProps = {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+}
+
+const CheckoutItem = ({name, imageUrl, price }: CheckoutItemProps) => {
+    return (
+    <div>
+        <img src={imageUrl} height={80} width={64} alt={name} title={name} />
+        <div>
+            <h3>{name}</h3>
+            <p>£{price.toFixed(2)}</p>
+        </div>
+    </div>
+    )
+}
+                          
+export default CheckoutItem;
